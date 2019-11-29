a987121f784fe43b146f64a60da4780d8f8549e1
Vishal-Kank <kankvishalkank@gmail.com>
2019-11-29 18:51:12 +0530

    Get Post By Id

--- routes/api/post.js
+++ routes/api/post.js
@@ -47,6 +47,24 @@ router.post('/',
     }
 );
 
+//--------------------------------------------------------------------------------------
+
+// @rout     :   GET api/posts
+// @desc     :   Get post by id
+// @access   :   Private
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) return res.status(404).json({ msg: 'Post not found!' });
+        return res.status(200).json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') return res.status(404).json({ msg: 'Post not found!' });
+        return res.status(500).send('Server Error!');
+    }
+});
+
 
 
 module.exports = router;
