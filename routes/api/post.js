f0cb6bd1dc7bc215aa42bc036c876237f837e0f6
Vishal-Kank <kankvishalkank@gmail.com>
2019-11-30 21:57:50 +0530

    Put likes to post

--- routes/api/post.js
+++ routes/api/post.js
@@ -107,4 +107,22 @@ router.delete('/:id', auth, async (req, res) => {
 });
 
 
+//--------------------------------------------------------------------------------------
+
+// @rout     :   Put api/posts/likes/:id
+// @desc     :   Put likes to post by user id
+// @access   :   Public
+router.put('/likes/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) return res.status(401).json({ msg: 'Post already liked!' });
+        post.likes.unshift({ user: req.user.id });
+        await post.save();
+        res.json(post);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error!');
+    }
+});
+
 module.exports = router;
