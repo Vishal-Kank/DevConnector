e1ef5b68ebd4d94ff24a3ef97b10fd7b2464af4b
Vishal-Kank <kankvishalkank@gmail.com>
2019-12-21 22:29:05 +0530

    Login page using react hooks.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     const [formData, setformData] = useState({
@@ -19,13 +20,15 @@ const Register = () => {
     */
 
     const { name, email, password, password2 } = formData;
+
     const onChange = e => setformData({ ...formData, [e.target.name]: e.target.value });
-    const onSubmit = e => {
+
+    const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
             console.log('Passwords do not match!');
         } else {
-            console.log(formData)
+            console.log('SUCCESS');
         }
     }
     return (
@@ -65,7 +68,7 @@ const Register = () => {
                     <input type="submit" className="btn btn-primary" value="Register" />
                 </form>
                 <p className="my-1">
-                    Already have an account? <a href="login.html">Sign In</a>
+                    Already have an account? <Link to="/login">Sign In</Link>
                 </p>
             </section>
         </Fragment>
