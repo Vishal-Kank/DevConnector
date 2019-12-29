c53c5c28e3b495ea9eb78559ea8863cfa6d89b90
Vishal-Kank <kankvishalkank@gmail.com>
2019-12-29 21:25:46 +0530

    Dashboard check for users not having profile.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_PROFILE } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -76,6 +76,6 @@ export const login = (email, password) => async dispatch => {
 
 //Logout & Clear Profile
 export const logout = () => dispatch => {
-    console.log('logout')
     dispatch({ type: LOGOUT });
+    dispatch({ type: CLEAR_PROFILE });
 };
