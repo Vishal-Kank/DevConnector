c7ec6a06d3ac691b672f0b41e2bac71236e1074e
Vishal-Kank <kankvishalkank@gmail.com>
2019-12-30 18:42:44 +0530

    Profile actio set.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { GET_PROFILE, PROFILE_ERROR, CREATE_PROFILE } from './types';
+//import setAuthToken from '../utils/setAuthToken';
 
 export const getCurrentProfile = () => async dispatch => {
     try {
@@ -11,6 +12,35 @@ export const getCurrentProfile = () => async dispatch => {
             payload: res.data
         });
     } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+}
+//Create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post('api/profile', formData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+        if (!edit) {
+            history.push('/dashboard');//redirected to dashboard
+        }
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
         dispatch({
             type: PROFILE_ERROR,
             payload: { msg: err.response.statusText, status: err.response.status }
