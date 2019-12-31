868ad25baf3591a7f9a8cb6c3ec74bc383bc5b07
Vishal-Kank <kankvishalkank@gmail.com>
2020-01-01 00:04:29 +0530

    Add Education

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 // Redux
 import { Provider } from 'react-redux';
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -86,7 +86,7 @@ export const addEducation = (formData, history) => async dispatch => {
                 'Content-Type': 'application/json'
             }
         }
-        const res = await axios.post('/api/profile/education', formData, config);
+        const res = await axios.put('/api/profile/education', formData, config);
 
         dispatch({
             type: UPDATE_PROFILE,
